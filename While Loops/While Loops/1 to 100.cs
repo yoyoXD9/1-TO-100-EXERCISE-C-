912e02379612e94f9a8859dbf59c90b70c0981b1
yoyoXD9 <168516103+yoyoXD9@users.noreply.github.com>
2024-04-30 09:43:24 +0300

    Add files via upload

---
+++ While Loops/While Loops/1 to 100.cs
@@ -0,0 +1,17 @@
+﻿namespace For_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+            }
+        }
+    }
+}
